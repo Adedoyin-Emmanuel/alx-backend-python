7a25ceb81431d3f383de09620656d9beb889d9ef
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-05 23:40:20 +0100

    Bug fix

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
+"""Write a type-annotated function add that takes a float a and a
+float b as arguments and returns their sum as a float.
 """
-Write a type-annotated function add that 
-takes a float a and a float b as arguments 
-and returns their sum as a float.
-"""
+
 
 def add(a: float, b: float) -> float:
-    """Adds two floating-point numbers and returns their sum.
-
-    Args:
-        a (float): The first floating-point number.
-        b (float): The second floating-point number.
-
-    Returns:
-        float: The sum of a and b as a floating-point number.
-    """
-    result = a + b
-    return result
+    """Add two floats"""
+    result = a + b 
+    return (result)
