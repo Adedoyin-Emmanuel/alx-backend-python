6c819f446bdc2d11cacb533d2fb960a7f41ba572
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-30 03:16:34 +0100

    Feat: Finished the 3rd task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 import unittest
 from parameterized import parameterized
 from typing import Dict, Tuple, Union
-from utils import access_nested_map
+from utils import (access_nested_map, get_json)
+from unittest.mock import patch, Mock
 
 """Create a test class that inherits from unittest.TestCase"""
 
@@ -38,6 +39,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """Tests `access_nested_map`'s exception raising."""
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """This tests the get_json function."""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
+        """This tests get_json's output."""
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
 
 
 if __name__ == '__main__':
