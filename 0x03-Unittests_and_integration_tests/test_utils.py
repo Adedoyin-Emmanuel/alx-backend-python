9c0bc73fbc12e89dbebbce56319c04761b1fe7ee
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-30 02:37:26 +0100

    Fix: Bug fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map  # Import the function to be tested
+from typing import Dict, Tuple, Union
+from utils import access_nested_map
 
 """Create a test class that inherits from unittest.TestCase"""
 
@@ -11,15 +12,18 @@ class TestAccessNestedMap(unittest.TestCase):
     to define multiple test cases
     """
     @parameterized.expand([
-        ({}, ("a",), "Key not found: 'a' in {}"),
-        ({"a": 1}, ("a", "b"), "Key not found: 'b' in {'a': 1}"),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map_exception(self, nested_map,
-                                         path, expected_exception_message):
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        actual_exception_message = str(context.exception)
-        self.assertEqual(actual_exception_message, expected_exception_message)
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
+        """Tests `access_nested_map`'s output."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 
 if __name__ == '__main__':
