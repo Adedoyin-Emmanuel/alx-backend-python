e63d9eec82e8de45b2b645fa2e0356010adbcb37
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-30 03:19:39 +0100

    Fix: Fixed pycodestyle errors

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 import unittest
 from parameterized import parameterized
 from typing import Dict, Tuple, Union
-from utils import (access_nested_map, get_json)
+from utils import (access_nested_map, get_json, memoize)
 from unittest.mock import patch, Mock
 
 """Create a test class that inherits from unittest.TestCase"""
@@ -57,6 +57,28 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**attrs)) as req_get:
             self.assertEqual(get_json(test_url), test_payload)
             req_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """This tests the memoize function."""
+    def test_memoize(self) -> None:
+        """This tests memoize's output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as memo_fxn:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_fxn.assert_called_once()
 
 
 if __name__ == '__main__':
